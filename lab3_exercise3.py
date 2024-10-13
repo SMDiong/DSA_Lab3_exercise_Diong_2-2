59510c7da05a6940be7f33c9b63d93298291e8be
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-10-13 19:07:16 +0800

    Added pseudocodes for Exercise 3

--- lab3_exercise3.py
+++ lab3_exercise3.py
@@ -3,3 +3,9 @@
 # October 13, 2024
 
 # Exercise 3: Provide a list comprehension that implement for a function called vowelsToUpper.
+
+# Define the vowelsToUpper function
+# Input the characters from the user
+# Input validation
+# Output the result
+# End
