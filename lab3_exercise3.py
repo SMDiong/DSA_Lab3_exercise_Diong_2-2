8fdd89498d86b542eb444709f788830ef2326bbf
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-10-13 19:16:05 +0800

    Finished Exercise 3 with a working code

--- lab3_exercise3.py
+++ lab3_exercise3.py
@@ -4,8 +4,16 @@
 
 # Exercise 3: Provide a list comprehension that implement for a function called vowelsToUpper.
 
-# Define the vowelsToUpper function
+# Define the vowelsToUpper function using with list comprehension
+def vowelsToUpper(user_string):
+    vowels = "aeiou"
+    return ''.join([ch.upper() if ch in vowels else ch for ch in user_string])
+
+
 # Input the characters from the user
-# Input validation
+user_input = input("Please enter a string: ")
+
 # Output the result
+print(user_input, "==", vowelsToUpper(user_input))
+
 # End
