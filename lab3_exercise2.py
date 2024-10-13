fa0b471b338e34cef5b1cce9d10e1bae0544d0ff
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-10-13 18:39:28 +0800

    Added pseudocodes for Exercise 2

--- lab3_exercise2.py
+++ lab3_exercise2.py
@@ -3,3 +3,11 @@
 # October 13, 2024
 
 # Exercise 2: Using comprehension, create a new list of tuples from two given lists:
+
+# Input the maximum number of players in the list
+# Create the list
+# Get the jersey number from the user
+# Get the jersey name from the user
+# Generate the list by using list comprehension from the user
+# Output the result
+# End
