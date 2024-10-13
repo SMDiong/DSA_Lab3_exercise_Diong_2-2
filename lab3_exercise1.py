943cd6a3d233934af722b2f3180a0d659fed48a1
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-10-13 18:23:28 +0800

    Finished Exercise 1 with a working code

--- lab3_exercise1.py
+++ lab3_exercise1.py
@@ -1,11 +1,19 @@
-## Diong, Shan Marc C.
+# Diong, Shan Marc C.
 # BSCpE 2-2
 # October 13, 2024
 
 # Exercise 1: From a given list of integers, create a new list using comprehension that will compute the square of odd integer elements.
 
 # Input the maximum number from the user
+max_number = int(input("Please enter the Maximum number in the list: "))
+
 # Generate the list from the user starting with 1
+number_list = list(range(1, max_number + 1))
+
 # Compute the square of the odd numbers by using list comprehension
+odd_squares = [x ** 2 for x in number_list if x % 2 != 0]
+
 # Output the result
+print("\nThe squares of all odd numbers in the list are: \n", odd_squares)
+
 # End
